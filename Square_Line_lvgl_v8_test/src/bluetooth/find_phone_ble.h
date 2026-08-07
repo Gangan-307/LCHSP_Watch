6323028c41159c54e48a95a1e044488e0ddca913
Gangan <2667497307@qq.com>
2026-08-07 15:30:05 +0800

    feat(app)：更新手表端将电量和充电状态同步至手机app

--- Square_Line_lvgl_v8_test/src/bluetooth/find_phone_ble.h
+++ Square_Line_lvgl_v8_test/src/bluetooth/find_phone_ble.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+#define HSP_WATCH_FIRMWARE_VERSION "0.1.0"
+
 /* Call after the Bluetooth stack has brought BLE up. Registers and advertises
  * the watch-owned companion GATT service. */
 void find_phone_ble_stack_ready(void);
@@ -14,6 +16,10 @@ void find_phone_ble_stack_ready(void);
 /* Start or stop the watch-to-phone alert. Calls are asynchronous. */
 void find_phone_ble_start(void);
 void find_phone_ble_stop(void);
+
+/* Publish the latest local power state to a subscribed Android companion. */
+void find_phone_ble_publish_device_status(uint8_t percent, uint8_t battery_valid,
+                                          uint8_t charging);
 
 /* Stop companion advertising before the Bluetooth radio is turned off. */
 void find_phone_ble_close(void);
